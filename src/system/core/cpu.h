125243f2779d3a3fb54b47f5b5aec0dc4158a19c
ewasion137 <formanban94@gmail.com>
2026-05-30 21:31:22 +0300

    Boot animation and makefile fixes

--- src/system/core/cpu.h
+++ src/system/core/cpu.h
@@ -31,4 +31,15 @@ static inline void write_cr4(uint64_t val) {
     __asm__ volatile("mov %0, %%cr4" : : "r"(val));
 }
 
+static inline uint64_t read_msr(uint32_t msr) {
+    uint32_t low, high;
+    __asm__ volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(msr));
+    return ((uint64_t)high << 32) | low;
+}
+
+static inline void write_msr(uint32_t msr, uint64_t val) {
+    uint32_t low = val & 0xFFFFFFFF;
+    uint32_t high = val >> 32;
+    __asm__ volatile("wrmsr" : : "a"(low), "d"(high), "c"(msr));
+}
 #endif
