1ec67d3767c66686f233bf5fb856956a4ac564d5
ewasion137 <formanban94@gmail.com>
2026-06-02 21:18:00 +0300

    PCI.h universal mapper MMIO for OHCI EHCI and xHCI

--- src/system/drivers/devices/pci/pci.h
+++ src/system/drivers/devices/pci/pci.h
@@ -1,9 +1,13 @@
 #ifndef PCI_H
 #define PCI_H
+
 #include <stdint.h>
 
 void pci_init(void);
 uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 void pci_write_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint16_t val);
+
+// Универсальный маппер MMIO для драйверов OHCI, EHCI, xHCI
+void *pci_map_mmio(uint64_t phys_addr, uint32_t size);
 
 #endif
