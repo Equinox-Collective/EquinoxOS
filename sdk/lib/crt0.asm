3d8363b2bbbb79032335834c6b8b1003bf6abe22
ewasion137 <formanban94@gmail.com>
2026-06-20 17:39:51 +0300

    Better crt0.asm (for c++)

--- sdk/lib/crt0.asm
+++ sdk/lib/crt0.asm
@@ -2,15 +2,15 @@
 [extern main]
 [extern exit]
 [extern __libc_init_env]
+[extern __libc_init_array]   ; Импортируем функцию вызова конструкторов C++
 [global _start]
 
 _start:
-    ; Ядро передаёт: rdi=argc, rsi=argv, rdx=envp (Этап 3).
+    ; Ядро передаёт: rdi=argc, rsi=argv, rdx=envp.
     ; 1. Выравниваем стек по 16 байтам (требование ABI)
     and rsp, -16
 
-    ; 2. Сохраняем argc/argv/envp в callee-saved регистры на время вызова
-    ;    инициализации libc (она их не трогает).
+    ; 2. Сохраняем argc/argv/envp в callee-saved регистры на время вызовов
     mov r13, rdi          ; argc
     mov r14, rsi          ; argv
     mov r15, rdx          ; envp
@@ -19,13 +19,16 @@ _start:
     mov rdi, r15
     call __libc_init_env
 
-    ; 4. Вызываем main(argc, argv, envp)
+    ; 4. Инициализируем глобальные конструкторы (C++)
+    call __libc_init_array
+
+    ; 5. Вызываем main(argc, argv, envp)
     mov rdi, r13
     mov rsi, r14
     mov rdx, r15
     call main
 
-    ; 5. Выход
+    ; 6. Выход
     mov rdi, rax
     call exit
 
